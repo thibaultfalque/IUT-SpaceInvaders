4cbac04a611e4da27f9e44f4970efab9d62b8b40
thibaultfalque <thibbault.falque@gmail.com>
2014-11-08 13:27:50 +0100

    mise à jour

--- base/Game.java
+++ base/Game.java
@@ -17,6 +17,7 @@ import javax.swing.JPanel;
 import niveau.Niveau1;
 import niveau.Niveau2;
 import niveau.Niveau3;
+import niveau.RandomMove;
 import niveau.UsineAlien;
 import entities.AlienEntity;
 import entities.Entity;
@@ -47,7 +48,7 @@ public class Game extends Canvas {
 	/** True if the game is currently "running", i.e. the game loop is looping */
 	private boolean gameRunning = true;
 	
-	private ArrayList<AlienEntity> entities=new ArrayList<AlienEntity>();
+	//private ArrayList<AlienEntity> entities=new ArrayList<AlienEntity>();
 	
 	/** The list of entities that need to be removed from the game this loop */
 	private ArrayList<Entity> removeList = new ArrayList<Entity>();
@@ -142,7 +143,7 @@ public class Game extends Canvas {
 	 */
 	private void startGame() {
 		// clear out any existing entities and intialise a new set
-		entities.clear();
+		ua.get(niveauCourant).getArrayAlien().clear();
 		missile.clear();
 		missile=new ArrayList<ShotEntity>();
 		removeList.clear();
@@ -162,7 +163,7 @@ public class Game extends Canvas {
 		// create the player ship and place it roughly in the center of the screen
 		ship = new ShipEntity(this,"sprites/ship.gif",370,550);
 		ua.get(niveauCourant).createArrayAlien();
-		entities=ua.get(niveauCourant).getArrayAlien();
+		//entities=ua.get(niveauCourant).getArrayAlien();
 		
 	}
 	
@@ -219,11 +220,14 @@ public class Game extends Canvas {
 		if (ua.get(niveauCourant).getAlienCount() == 0) {
 			notifyWin();
 		}
-		
 		// if there are still some aliens left then they all need to get faster, so
 		// speed up all the existing aliens
-		for(AlienEntity entity : entities) 
+		for(AlienEntity entity : ua.get(niveauCourant).getArrayAlien()){
 			entity.setHorizontalMovement(entity.getHorizontalMovement() * 1.02);
+			//S'il reste moins de 5 aliens on change la strategie de déplacement
+			if(ua.get(niveauCourant).getAlienCount()<=5)
+				entity.setStrategieMove(new RandomMove());
+		}
 		
 	}
 	
@@ -274,7 +278,7 @@ public class Game extends Canvas {
 			
 			// cycle round asking each entity to move itself
 			if (!waitingForKeyPress) {
-				for(AlienEntity ae : entities)
+				for(AlienEntity ae : ua.get(niveauCourant).getArrayAlien())
 					ae.move(delta);
 				for(ShotEntity m : missile)
 					m.move(delta);
@@ -282,7 +286,7 @@ public class Game extends Canvas {
 			}
 			
 			// cycle round drawing all the entities we have in the game
-            for(AlienEntity entity : entities)
+            for(AlienEntity entity : ua.get(niveauCourant).getArrayAlien())
 				entity.draw(g);
             if(missile!=null)
             	for(ShotEntity m:missile)
@@ -305,12 +309,12 @@ public class Game extends Canvas {
 				}*/
 				if(missile!=null){
 					for(ShotEntity m:missile)
-						for(AlienEntity ae:entities){
+						for(AlienEntity ae:ua.get(niveauCourant).getArrayAlien()){
 							
 							m.collidedWith(ae);
 							ae.collidedWith(m);
 						}
-					for(AlienEntity ae:entities)
+					for(AlienEntity ae:ua.get(niveauCourant).getArrayAlien())
 						ship.collidedWith(ae);
 				}
 				
@@ -319,7 +323,7 @@ public class Game extends Canvas {
 			
 			if(!removeList.isEmpty()){
 				System.out.println("REMOVE");
-				entities.removeAll(removeList);
+				ua.get(niveauCourant).getArrayAlien().removeAll(removeList);
 				missile.removeAll(removeList);
 				removeList.clear();
 			}
@@ -328,7 +332,7 @@ public class Game extends Canvas {
 			// be resolved, cycle round every entity requesting that
 			// their personal logic should be considered.
 			if (logicRequiredThisLoop) {
-			    for(Entity entity : entities) {
+			    for(Entity entity : ua.get(niveauCourant).getArrayAlien()) {
 					entity.doLogic();
 				}
 				logicRequiredThisLoop = false;
